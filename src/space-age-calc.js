840d64ce3b849117ce143c264133e374376d6654
Heather Zurek <heather.zurek@gmail.com>
2019-05-31 14:06:17 -0700

    write in plain english

--- src/space-age-calc.js
+++ src/space-age-calc.js
@@ -1,21 +1,10 @@
-function CalculateAge(year, month, day) {
-  let dateNumber = dateInput.getUTCDay();
-  let dayOfTheWeek = "";
+//get users date of birth as input
+//get current date
+//find their age
 
-
-  let currentDate = new Date();
-  let currentYear = currentDate.getFullYear();
-  let currentMonth = currentDate.getUTCMonth() + 1;
-  let currentDay = currentDate.getUTCDate();
-  let age = currentYear - year;
-  if (currentMonth > month) {
-    return age;
-  } else {
-    if (currentDay >= day) {
-      return age;
-    } else {
-      age--;
-      return age;
-    }
-  }
-}
+//divide their age by mercury years
+//divide their age by venus years
+//divide their age by mars years
+//divide their age by Jupiter years
+//calculate users life expectancy
+//return number of years theyve lived past their life expectancy
