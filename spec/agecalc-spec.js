7147acd37208d933d6dc867fc31bd4c66d0c5049
Lina Shadrach <lina.shadrach@gmail.com>
2019-06-03 11:34:57 -0700

    stuff we did

--- spec/agecalc-spec.js
+++ spec/agecalc-spec.js
@@ -3,48 +3,55 @@ import { ageCalc } from './../src/agecalc.js';
 describe('ageCalc', function() {
 
   it('should test correct age on earth', function() {
-    let testDate = new ageCalc(new Date(1990, 2, 13));
-    let dob = testDate.birthday;
-    let newAge = testDate.ageOnEarth(dob);
+    let testDate = new ageCalc(1990, 2, 13);
+    // let dob = testDate.birthday;
+    let newAge = testDate.ageOnEarth();
 
     expect(newAge).toEqual(29);
     expect(newAge).not.toEqual(32);
   });
 
   it('should test users correct age on mercury', function() {
-    let testDate = new ageCalc(new Date(1990, 2, 13));
-    let dob = testDate.birthday;
-    let mercuryAge = testDate.ageOnMercury(dob);
+    let testDate = new ageCalc(1990, 2, 13);
+    // let dob = testDate.birthday;
+    let mercuryAge = testDate.ageOnMercury();
 
     expect(mercuryAge).toEqual(120);
     expect(mercuryAge).not.toEqual(99);
   });
 
   it('should test users correct age on venus', function() {
-    let testDate = new ageCalc(new Date(1990, 2, 13));
-    let dob = testDate.birthday;
-    let venusAge = testDate.ageOnVenus(dob);
+    let testDate = new ageCalc(1990, 2, 13);
+    // let dob = testDate.birthday;
+    let venusAge = testDate.ageOnVenus();
 
     expect(venusAge).toEqual(46);
     expect(venusAge).not.toEqual(66);
   });
 
   it('should test users correct age on mars', function() {
-    let testDate = new ageCalc(new Date(1990, 2, 13));
-    let dob = testDate.birthday;
-    let marsAge = testDate.ageOnMars(dob);
+    let testDate = new ageCalc(1990, 2, 13);
+    // let dob = testDate.birthday;
+    let marsAge = testDate.ageOnMars();
 
     expect(marsAge).toEqual(15);
     expect(marsAge).not.toEqual(51);
   });
 
   it('should test users correct age on mars', function() {
-    let testDate = new ageCalc(new Date(1990, 2, 13));
-    let dob = testDate.birthday;
-    let jupiterAge = testDate.ageOnJupiter(dob);
+    let testDate = new ageCalc(1990, 2, 13);
+    // let dob = testDate.birthday;
+    let jupiterAge = testDate.ageOnJupiter();
 
     expect(jupiterAge).toEqual(2);
     expect(jupiterAge).not.toEqual(34);
+  });
+
+  it('should return years to live based on life expectancy of 78 in Mercury', function() {
+    let testDate = new ageCalc(1990, 2, 13);
+    let yearsLeftOnMercury = testDate.yearsLeftOnPlanet("Mercury");
+
+    expect(yearsLeftOnMercury).toEqual(205);
   });
 
 });
--- src/agecalc.js
+++ src/agecalc.js
@@ -1,41 +1,61 @@
-class ageCalc {
-  constructor(birthdate) {
-    this.birthdate = new Date(birthdate);
+const planetConstants = {
+  "Mercury": .24,
+  "Venus": .64,
+  "Mars": 1.88,
+  "Jupiter": 11.86,
+}
+
+export class ageCalc {
+  constructor(year, month, day) {
+    this.birthdate = new Date(year, month, day);
+    // this.planetConstants = {
+    //   "Mercury": .24,
+    //   "Venus": .64,
+    //   "Mars": 1.88,
+    //   "Jupiter": 11.86,
+    // }
+  }
+
+  yearsLeftOnPlanet(planet) {
+    const lifeExpectancyOnEarth = 78;
+    let lifeExpectancyOnPlanet = Math.floor(lifeExpectancyOnEarth / this.planetConstants[planet]);
+    let yearsLeft = lifeExpectancyOnPlanet - this.ageOnPlanet(planet);
+    return yearsLeft;
   }
 
   ageOnEarth() {
     let today = new Date();
     let year = today.getFullYear();
     let age = year - (this.birthdate.getFullYear());
-    let m = today.getMonth() - (this.birthdate.getMonth());
-    if (m < 0 || (m === 0 && today.getDate() < (this.birthdate.getDate()))) {
+    let month = today.getMonth() - (this.birthdate.getMonth());
+    if (month < 0 || (month === 0 && today.getDate() < (this.birthdate.getDate()))) {
       age--;
     }
     return age;
   }
 
-  ageOnMercury() {
+  ageOnPlanet(planet) {
     let age = this.ageOnEarth();
-    let mercuryAge = Math.floor(age / .24);
+    let mercuryAge = Math.floor(age / this.planetConstants[planet]);
     return mercuryAge;
   }
 
-  ageOnVenus() {
-    let age = this.ageOnEarth();
-    let venusAge = Math.floor(age / .62);
-    return venusAge;
-  }
+  // ageOnVenus() {
+  //   let age = this.ageOnEarth();
+  //   let venusAge = Math.floor(age / .62);
+  //   return venusAge;
+  // }
 
-  ageOnMars() {
-    let age = this.ageOnEarth();
-    let marsAge = Math.floor(age / 1.88);
-    return marsAge
-  }
+  // ageOnMars() {
+  //   let age = this.ageOnEarth();
+  //   let marsAge = Math.floor(age / 1.88);
+  //   return marsAge
+  // }
 
-  ageOnJupiter() {
-    let age = this.ageOnEarth();
-    let jupiterAge = Math.floor(age / 11.86);
-    return jupiterAge;
+  // ageOnJupiter() {
+  //   let age = this.ageOnEarth();
+  //   let jupiterAge = Math.floor(age / 11.86);
+  //   return jupiterAge;
   }
 
 
@@ -43,6 +63,6 @@ class ageCalc {
   // jupiter 11.86
 
 }
-module.exports = {
-  ageCalc:ageCalc
-}
+// module.exports = {
+//   ageCalc:ageCalc
+// }
